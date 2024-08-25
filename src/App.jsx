fa7fc61b0d539889ef75face195b567afff82860
Prasanna Gaikwad <114876414+Prasanna018@users.noreply.github.com>
2024-08-25 21:21:22 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1);
+
+  }
+  function decrement() {
+
+    setCount(count - 1);
+  }
+
+  return (
+
+    <div>
+      <p>count : {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+
+
+    </div>
+  )
+}
+
+export default App
